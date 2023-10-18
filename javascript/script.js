e2def81ac981e37dc2af5b7b48a9eea07454fee1
anderss-oj <123219287+anderss-oj@users.noreply.github.com>
2023-10-17 20:11:00 -0400

    javascript practice

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+function change() {
+    var menu = document.getElementById("menu");
+    var spinner = document.getElementById("spinner")
+    var backgroundImage = document.getElementById('background-image')
+
+    menu.classList.toggle("active");
+    spinner.classList.toggle("spin");
+    document.body.classList.toggle("dark");
+    backgroundImage.classList.toggle("transparent")
+}
